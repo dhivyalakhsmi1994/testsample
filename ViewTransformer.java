1e4cdd006b65863f5436c1d2d11d15bd19e0677f
niya <niya@es07>
2017-11-09 15:50:53 +0530

    nithtya edited dhvya project

--- ViewTransformer.java
+++ ViewTransformer.java
@@ -3,19 +3,7 @@ package systems.ellora.curie.api.Externals.BottomSheet;
 import android.view.View;
 
 
-public interface View{
     
-
-    /**
-     * Called on every frame while animating the presented sheet. This method allows you to coordinate
-     * other animations (usually on the content view) with the sheet view's translation.
-     *
-     * @param translation The current translation of the presented sheet view.
-     * @param maxTranslation The max translation of the presented sheet view.
-     * @param peekedTranslation The peeked state translation of the presented sheet view.
-     * @param parent The BottomSheet presenting the sheet view.
-     * @param view The content view to transform.
-     */
     void transformView(float translation, float maxTranslation, float peekedTranslation, BottomSheetLayout parent, View view);
 
     /**
