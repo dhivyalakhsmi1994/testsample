032d85f6887a2a7132fa93e5b708e42f83a54d1b
niya <niya@es07>
2017-11-09 17:07:17 +0530

    liked

--- ViewTransformer.java
+++ ViewTransformer.java
@@ -6,7 +6,7 @@ import android.view.View;
     
     void transformView(float translation, float maxTranslation, float peekedTranslation, BottomSheetLayout parent, View view);
 
-
+gkjkjgksjgfkjgfkjsgksfgkjsfgreh
 
     float getDimAlpha(float translation, float maxTranslation, float peekedTranslation, BottomSheetLayout parent, View view);
 // uesful all
